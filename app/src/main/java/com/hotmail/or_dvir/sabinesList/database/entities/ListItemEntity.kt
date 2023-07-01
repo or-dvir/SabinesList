0a6064d06c7459a0df77cc004608b104ca556053
Or Dvir <or_dvir@hotmail.com>
2023-07-01 22:05:04 +0200

    added index for ListItemEntity

--- app/src/main/java/com/hotmail/or_dvir/sabinesList/database/entities/ListItemEntity.kt
+++ app/src/main/java/com/hotmail/or_dvir/sabinesList/database/entities/ListItemEntity.kt
@@ -4,9 +4,11 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.ForeignKey.Companion.CASCADE
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
+    indices = [Index(ListItemEntity.COLUMN_LIST_ID)],
     tableName = ListItemEntity.TABLE_NAME,
     foreignKeys = [ForeignKey(
         entity = UserListEntity::class,
