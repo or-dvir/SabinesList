0d13d527afbf0ddf8d8af884065e7b8a4c6403dd
Or Dvir <or_dvir@hotmail.com>
2023-08-17 14:47:06 +0200

    fixed typos

--- app/src/main/java/com/hotmail/or_dvir/sabinesList/ui/theme/Theme.kt
+++ app/src/main/java/com/hotmail/or_dvir/sabinesList/ui/theme/Theme.kt
@@ -12,14 +12,14 @@ private val colorPrimaryVariant = Color(0xff009867)
 private val colorSecondary = Color(0xffB43D6B)
 private val colorStatusBarDark = Color(0xff0e0e0e)
 
-private val LightColorPalette = lightColors(
+private val lightColorPalette = lightColors(
     primary = Color(0xff3db486),
     primaryVariant = colorPrimaryVariant,
     secondary = colorSecondary,
     secondaryVariant = Color(0xff773256)
 )
 
-private val DarkColorPalette = darkColors(
+private val darkColorPalette = darkColors(
     primary = Color(0xff93d2b6),
     primaryVariant = colorPrimaryVariant,
     secondary = colorSecondary,
@@ -29,9 +29,9 @@ private val DarkColorPalette = darkColors(
 @Composable
 fun SabinesListTheme(darkTheme: Boolean, content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        darkColorPalette
     } else {
-        LightColorPalette
+        lightColorPalette
     }
 
     val systemUiController = rememberSystemUiController()
